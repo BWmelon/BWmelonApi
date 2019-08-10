48a085117112f8a7cd6085006c98b09c7fd4cec6
bwmelon <1454490647@qq.com>
2019-08-10 16:34:28 +0800

    :sparkles:新增网易云api

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-// console.log(app);
 
 const path = require('path');
 const fs = require('fs');
@@ -18,6 +17,7 @@ const qqinfo = require("./routes/api/qqinfo");
 const sitetitle = require("./routes/api/sitetitle");
 const icp = require("./routes/api/icp.js");
 const onenote = require("./routes/api/onenote.js");
+const cloudmusic = require("./routes/api/cloudmusic.js");
 const bing = require("./routes/api/bing.js");
 
 app.use("/api/tinyurl", tinyurl);
@@ -28,16 +28,21 @@ app.use("/api/qqinfo", qqinfo);
 app.use("/api/sitetitle", sitetitle);
 app.use("/api/icp", icp);
 app.use("/api/onenote", onenote);
+app.use("/api/cloudmusic", cloudmusic);
 app.use("/api/bing", bing);
 
-const httpsOption = {
-    cert: fs.readFileSync("./certificate/xxxxx.pem"),
-    key: fs.readFileSync("./certificate/xxxxx.key")
-}
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Sever running on port ${port}`);
 });
 
-https.createServer(httpsOption, app).listen(444);
+
+
+// https配置
+
+// const httpsOption = {
+//     cert: fs.readFileSync("./certificate/xxxxx.pem"),
+//     key: fs.readFileSync("./certificate/xxxxx.key")
+// }
+// https.createServer(httpsOption, app).listen(444);
